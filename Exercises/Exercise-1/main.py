8e71d79622e239392ebff1bed090d02b91158386
lisa <abokhailisa@gmail.com>
2022-06-03 06:29:58 +0100

    Exercise 1 code

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
 import requests
+import os, zipfile
+from io import BytesIO
+
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -13,7 +16,26 @@ download_uris = [
 
 def main():
     # your code here
-    pass
+
+    def extract(download):
+        directory_name = 'downloads'
+        if not os.path.exists(directory_name):   # Make downloads folder if it does not exist
+            os.makedirs(directory_name)
+        
+        for uri in download:
+            filename= uri.split('/')[-1]  # Extract filename from url 
+            file_path = os.path.join(directory_name, filename)
+            r = requests.get(uri)  
+            if r.ok: 
+                    print(f'Downloading {filename} into {file_path}')
+                    uri_zip= zipfile.ZipFile(BytesIO(r.content)) # Extract csv file from dowloaded zip file
+                    uri_zip.extractall(file_path)
+            else:
+                print(f'Download failed with {filename}')
+
+
+    extract(download_uris)
+
 
 
 if __name__ == '__main__':
