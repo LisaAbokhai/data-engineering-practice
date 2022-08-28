7b9223df94a642d6492c4fdb5837b37589aea20a
lisa <abokhailisa@gmail.com>
2022-08-28 21:05:34 +0100

    Exercise 2 done

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,41 @@
-import requests
-import pandas
+import pandas as pd
+from requests import get
+from bs4 import BeautifulSoup as bs
+import io
 
+
+# assign the url 
+url_download = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
 
 
 def main():
-    # your code here
-    pass
+    # your code here 
+    response = get(url_download)
+    soup = bs(response.text, 'html.parser')
+    files= soup.find_all('tr')
+    
+    # fetch the csv file based  on the date
+    for file in files:
+        if '2022-02-07 14:03' in file.get_text():
+            download = file.a['href']
+            break
+    
+    # create the link to the csv
+    download_site = url_download + download
+
+    # Turn the csv file into a dataframe
+    csv_df = pd.read_csv(download_site, header= 0)
+
+    #  find the max valyue in HourlyDryBulbTemperature col and return the records    
+    print(csv_df.iloc[csv_df['HourlyDryBulbTemperature'].idxmax()])
+    print(csv_df.query('HourlyDryBulbTemperature == HourlyDryBulbTemperature.max()'))
+    print(csv_df.nlargest(1,['HourlyDryBulbTemperature']))
+
+    
+
+
+    
+
 
 
 if __name__ == '__main__':
